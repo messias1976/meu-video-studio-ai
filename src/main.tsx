bc564a069bd154fb321270a33a4329bf3fa3af06
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 14:08:13 -0300

    fix: ativa compositor persistente de camadas

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import './editor-controls.css'
 import './phase5.js'
 import './timeline-sync.ts'
-import './multilayer-compositor.ts'
+import './multilayer-compositor-v2.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
