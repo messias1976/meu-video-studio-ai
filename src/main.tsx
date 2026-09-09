501192ef4bdf0bb962627836fed5bd29ca098d1c
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 14:53:01 -0300

    refactor: ativa novo compositor de preview em camadas

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './editor-controls.css'
 import './phase5.js'
-import './multilayer-compositor-v2.ts'
+import './layered-preview-v3.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
