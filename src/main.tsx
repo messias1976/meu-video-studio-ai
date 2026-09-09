6b9993b18ba811cacde44ca7eafa09fc6b9df67b
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 13:41:45 -0300

    feat: ativa compositor de camadas no editor

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import './index.css'
 import './editor-controls.css'
 import './phase5.js'
 import './timeline-sync.ts'
-import './editor-view-controls.ts'
-import './transform-panel-drag.ts'
+import './multilayer-compositor.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
