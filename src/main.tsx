67889f975bac66a3e9591ee33e55b656dd32a800
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 11:25:17 -0300

    fix: ativar timeline dinamica

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './phase5.js'
+import './timeline-sync.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
