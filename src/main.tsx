2410fb2c07e6ba79b9cd53bcbfc10d20281bd82e
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 12:31:54 -0300

    feat: ativa controles de visualização do editor

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import './editor-controls.css'
 import './phase5.js'
 import './timeline-sync.ts'
+import './editor-view-controls.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
