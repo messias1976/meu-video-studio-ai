92252449b630d7f489a4105d2fe01e2517666c28
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 12:49:56 -0300

    feat: adiciona painel de transformação movel

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './editor-controls.css'
 import './phase5.js'
 import './timeline-sync.ts'
 import './editor-view-controls.ts'
+import './transform-panel-drag.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
