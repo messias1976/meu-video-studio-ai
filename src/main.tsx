ffd8582ae5de93bc6ded4aada0784ca677632e39
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 14:22:28 -0300

    fix: unifica sincronizacao da timeline e compositor

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './editor-controls.css'
 import './phase5.js'
-import './timeline-sync.ts'
 import './multilayer-compositor-v2.ts'
 import App from './App.tsx'
 
