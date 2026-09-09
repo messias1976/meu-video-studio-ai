bd93b98cf1418346a375d4a28720f76442e5aaee
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 15:02:37 -0300

    fix: ativa compositor v5 para camadas

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './editor-controls.css'
 import './phase5.js'
-import './layered-preview-v3.ts'
+import './layered-preview-v5.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
