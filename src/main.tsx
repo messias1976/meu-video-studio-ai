68e5f4f5ea94c1fda8d23003df083be212a55505
Messias Machado <messiasmachado1976@gmail.com>
2026-09-09 11:33:39 -0300

    style: ativar controles visuais do editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './editor-controls.css'
 import './phase5.js'
 import './timeline-sync.ts'
 import App from './App.tsx'
