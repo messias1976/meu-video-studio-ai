24e773ab875bd6da5a21a192d8519bee73d626c4
Messias Machado <messiasmachado1976@gmail.com>
2026-09-11 13:50:12 -0300

    feat: integra renderizador de exportação

--- src/boot.tsx
+++ src/boot.tsx
@@ -5,6 +5,7 @@ import ProjectSettings from './studio/ProjectSettings'
 import ProjectNameEditor from './studio/ProjectNameEditor'
 import TimelineInteractions from './studio/timeline-interactions'
 import PreviewComposition from './studio/preview-composition'
+import ExportController from './studio/ExportController'
 import './studio/preview-clean.css'
 import './studio/window-fit.css'
 
@@ -18,4 +19,4 @@ class RuntimeErrorBoundary extends React.Component<React.PropsWithChildren, { er
 }
 const root=document.getElementById('root')
 if(!root) throw new Error('Elemento #root não encontrado')
-createRoot(root).render(<React.StrictMode><RuntimeErrorBoundary><><StudioApp/><ProjectSettings/><ProjectNameEditor/><TimelineInteractions/><PreviewComposition/></></RuntimeErrorBoundary></React.StrictMode>)
+createRoot(root).render(<React.StrictMode><RuntimeErrorBoundary><><StudioApp/><ProjectSettings/><ProjectNameEditor/><TimelineInteractions/><PreviewComposition/><ExportController/></></RuntimeErrorBoundary></React.StrictMode>)
