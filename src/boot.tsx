7fd1c262f6cff0acc52361410ced5f9d0ea1929c
Messias Machado <messiasmachado1976@gmail.com>
2026-09-11 10:37:52 -0300

    style: enable final studio overrides

--- src/boot.tsx
+++ src/boot.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import StudioApp from './studio/StudioAppFinal'
+import './studio/final-overrides.css'
 
 class RuntimeErrorBoundary extends React.Component<React.PropsWithChildren, { error: Error | null }> {
   state = { error: null as Error | null }
